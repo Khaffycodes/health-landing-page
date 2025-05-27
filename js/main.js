8bea55d187660569bc9c15b2fdcc6c1afab2c99d
Salau Kafayat <shilekhaffy@gmail.com>
2025-05-27 17:39:14 +0100

    building hero section

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//mobile nav
+const mobileNav = document.querySelector('.mnav');
+const closeBtn = document.querySelector('.mnav__close-btn');
+const closeBtnIcon = document.querySelector('.mnav__close-btn-icon');
+
+const navOpenedClass = 'left-[0px]';
+const navClosedClass = '-left-[300px]';
+const arrowLeftClass = 'ri-arrow-left-s-line';
+const arrowRightClass = 'ri-arrow-right-s-line';
+
+closeBtn.addEventListener('click', () => {
+  if (mobileNav.classList.contains(navClosedClass)) {
+    mobileNav.classList.remove(navClosedClass);
+    mobileNav.classList.add(navOpenedClass);
+  } else {
+    mobileNav.classList.remove(navOpenedClass);
+    mobileNav.classList.add(navClosedClass);
+  }
+  closeBtnIcon.classList.toggle(arrowLeftClass);
+  closeBtnIcon.classList.toggle(arrowRightClass);
+});
